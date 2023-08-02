036afe0ea705af39e7b6eb62291e8ec95b7a886b
hongsh <hongsh@the-51.com>
2023-08-02 13:39:37 +0900

    [로그인 페이지]

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    index
+  <div class="wrap login-page">
+    <div class="login-img"></div>
+    <div class="login-container">
+      
+    </div>
   </div>
 </template>
+
+<style lang="sass">
+
+</style>
