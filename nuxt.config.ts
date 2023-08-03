c15c87db08ba98a40a240eda8328f31dd2d84df2
honghao30 <sunghoo78@gmail.com>
2023-08-03 23:51:51 +0900

    pc버전~

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const AutoImport = require('unplugin-auto-import/webpack')
-const Components = require('unplugin-vue-components/webpack')
-const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
-
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
     // CSS file in the project
     '@/assets/scss/index.scss',
   ],
-  transpileDependencies: true,
-  lintOnSave:false,
-  configureWebpack: {
-    plugins: [
-      AutoImport({
-        resolvers: [ElementPlusResolver()],
-      }),
-      Components({
-        resolvers: [ElementPlusResolver()],
-      }),
-    ],
-  }
+  modules: ['@element-plus/nuxt']
 })
--- pages/sub/order_history.vue
+++ pages/sub/order_history.vue
@@ -55,17 +55,37 @@
           </div>
         </div>                
         <div class="cont">
-
+          <h3 class="sub-title">에코해운</h3>
           <div class="scroll__wrapper">
             <el-table :data="tableData" style="width: 100%" height="250">
-              <el-table-column label="Address Info" fixed="left">
-                <el-table-column prop="state" label="State" width="100" />
-                <el-table-column prop="city" label="City" width="120" />
-                <el-table-column prop="address" label="Address" width="250" />
+              <el-table-column label="기본정보" fixed="left">
+                <el-table-column prop="num" label="이용번호" width="100" />
+                <el-table-column prop="orderDate" label="주문일시" width="120" />
+                <el-table-column prop="company" label="사업자명" width="100" />
               </el-table-column>
-              <el-table-column prop="date" label="Date" width="150" />
-              <el-table-column prop="name" label="Name" width="150" />
-              <el-table-column prop="zip" label="Zip" width="150" />
+              <el-table-column prop="part" label="부서명" width="100" />
+              <el-table-column prop="Name" label="직원명" width="100" />
+              <el-table-column prop="porsonId" label="직원ID" width="150" />
+              <el-table-column prop="email" label="직원이메일" width="250" />
+              <el-table-column prop="tell" label="직원연락처" width="150" />
+              <el-table-column prop="port" label="항구명" width="100" />
+              <el-table-column prop="ship" label="선박명" width="150" />
+              <el-table-column prop="portArea" label="묘박지" width="80" />
+              <el-table-column prop="shiptype" label="선종" width="150" />
+              <el-table-column prop="data2" label="배송희망일시" width="150" />
+              <el-table-column prop="tel2" label="본선연락처" width="150" />
+              <el-table-column prop="product" label="품목" width="100" />
+              <el-table-column prop="couter" label="수량" width="80" />
+              <el-table-column prop="cout" label="수량" width="80" />
+              <el-table-column prop="unit" label="단위" width="80" />
+              <el-table-column prop="weit" label="무게" width="80" />
+              <el-table-column prop="plusPrice" label="화물가액" width="80" />
+              <el-table-column prop="addres" label="출발지 주소" width="280" />
+              <el-table-column prop="flyTime" label="비행 스케줄 안내 일시" width="150" />
+              <el-table-column prop="flyTime2" label="비행예정일시" width="150" />
+              <el-table-column prop="status" label="배송상태" width="100" />
+              <el-table-column prop="discout" label="할인적용율" width="100" />
+              <el-table-column prop="fileup" label="서류 업로드" width="100" />
             </el-table>          
 
 
