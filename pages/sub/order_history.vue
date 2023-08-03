6144045b4f2175cc07bebb06d58039ff331a2949
hongsh <hongsh@the-51.com>
2023-08-03 14:35:47 +0900

    마크업만

--- pages/sub/order_history.vue
+++ pages/sub/order_history.vue
@@ -44,6 +44,21 @@
         <h2 class="page-title">Order History</h2>
         <div class="cont">
 
+          <div class="scroll__wrapper">
+            <el-table :data="tableData" style="width: 100%" height="250">
+              <el-table-column label="Address Info" fixed="left">
+                <el-table-column prop="state" label="State" width="100" />
+                <el-table-column prop="city" label="City" width="120" />
+                <el-table-column prop="address" label="Address" width="250" />
+              </el-table-column>
+              <el-table-column prop="date" label="Date" width="150" />
+              <el-table-column prop="name" label="Name" width="150" />
+              <el-table-column prop="zip" label="Zip" width="150" />
+            </el-table>          
+
+
+          </div>
+          <!-- // 스크롤 -->
 
           
         </div>
@@ -62,10 +77,65 @@
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script lang="ts" setup>
+  const tableData = [
+    {
+      date: '2016-05-03',
+      name: 'Tom',
+      state: 'California',
+      city: 'Los Angeles',
+      address: 'No. 189, Grove St, Los Angeles',
+      zip: 'CA 90036',
+    },
+    {
+      date: '2016-05-02',
+      name: 'Tom',
+      state: 'California',
+      city: 'Los Angeles',
+      address: 'No. 189, Grove St, Los Angeles',
+      zip: 'CA 90036',
+    },
+    {
+      date: '2016-05-04',
+      name: 'Tom',
+      state: 'California',
+      city: 'Los Angeles',
+      address: 'No. 189, Grove St, Los Angeles',
+      zip: 'CA 90036',
+    },
+    {
+      date: '2016-05-01',
+      name: 'Tom',
+      state: 'California',
+      city: 'Los Angeles',
+      address: 'No. 189, Grove St, Los Angeles',
+      zip: 'CA 90036',
+    },
+    {
+      date: '2016-05-08',
+      name: 'Tom',
+      state: 'California',
+      city: 'Los Angeles',
+      address: 'No. 189, Grove St, Los Angeles',
+      zip: 'CA 90036',
+    },
+    {
+      date: '2016-05-06',
+      name: 'Tom',
+      state: 'California',
+      city: 'Los Angeles',
+      address: 'No. 189, Grove St, Los Angeles',
+      zip: 'CA 90036',
+    },
+    {
+      date: '2016-05-07',
+      name: 'Tom',
+      state: 'California',
+      city: 'Los Angeles',
+      address: 'No. 189, Grove St, Los Angeles',
+      zip: 'CA 90036',
+    },
+  ]
 </script>
 
 <style>
