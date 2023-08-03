4aacf672043ebdd0ab10d259fc1c5841bad3026a
hongsh <hongsh@the-51.com>
2023-08-03 14:48:35 +0900

    엘리먼트

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
     // CSS file in the project
     '@/assets/scss/index.scss',
   ],
+  transpileDependencies: true,
+  lintOnSave:false,
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  }
 })
