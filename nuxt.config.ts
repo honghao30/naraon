1a7c536ce2eabc89ec9aad86fa0c9c3b3db9f218
hongsh <hongsh@the-51.com>
2023-08-04 13:53:25 +0900

    메타태그 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,15 @@ export default defineNuxtConfig({
     // CSS file in the project
     '@/assets/scss/index.scss',
   ],
+  app: {
+    head: {
+      charset: 'utf-8',
+      title: 'Naraon',
+      meta: [
+        {name: 'description', content: ''},
+        {name: 'author', content: ''}
+      ]
+    }
+  },
   modules: ['@element-plus/nuxt']
 })
